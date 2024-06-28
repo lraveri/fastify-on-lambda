c6b2cf3ea94a92ef0dfa53461844e8a82d2a6658
Luca Raveri <lucaraveri993@gmail.com>
2024-06-28 15:44:43 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const fastify = require('fastify')
+
+const app = fastify()
+app.get('/hello', (request, reply) => {
+    reply.send({ message: 'Hello from AWS Lambda!' })
+})
+
+app.post('/hello', (request, reply) => {
+    reply.send({ message: `Hello ${request.body.name} from AWS Lambda!`})
+})
+
+if (require.main === module) {
+    // called directly i.e. "node app"
+    app.listen({ port: 3000 }, (err) => {
+        if (err) console.error(err)
+        console.log('server listening on 3000')
+    })
+} else {
+    // required as a module => executed on aws lambda
+    module.exports = app
+}
---
+++ lambda.js
@@ -0,0 +1,14 @@
+const awsLambdaFastify = require('@fastify/aws-lambda')
+const app = require('./app')
+
+const proxy = awsLambdaFastify(app)
+// or
+// const proxy = awsLambdaFastify(app, { binaryMimeTypes: ['application/octet-stream'], serializeLambdaArguments: false /* default is true */ })
+
+exports.handler = proxy
+// or
+// exports.handler = (event, context, callback) => proxy(event, context, callback)
+// or
+// exports.handler = (event, context) => proxy(event, context)
+// or
+// exports.handler = async (event, context) => proxy(event, context)
